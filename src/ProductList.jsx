949806c030436760e38c51534455039bcc6527a3
Ralphdany <ralphnjambou@gmail.com>
2025-08-31 10:03:56 -0400

    cart item number bug fixed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,10 +6,11 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
-    const [itemCart, setItemCart] = useState(0)
+    // const [itemCart, setItemCart] = useState(0)
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch()
+    const cartItems = useSelector(state => state.cart.items)
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -265,7 +266,7 @@ function ProductList({ onHomeClick }) {
         ...prevState,
         [product.name]: true,
       }))
-      setItemCart((prevState) => prevState + 1)
+    //   setItemCart((prevState) => prevState + 1)
     }
     return (
         <div>
@@ -287,7 +288,7 @@ function ProductList({ onHomeClick }) {
                     <div style={{position: "relative"}}> 
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                             <h1 className='cart'>
-                                <small style={{position: "absolute", top:"35%", left: "50%", fontSize: ".8rem"}}>{itemCart}</small>
+                                <small style={{position: "absolute", top:"35%", left: "50%", fontSize: ".8rem"}}>{cartItems.length}</small>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
                             </svg>
                             </h1>
